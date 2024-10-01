ea706228f04687135211a545d162b1eeed02b77a
ndkode <nandang.k95@gmail.com>
2024-10-01 21:06:22 +0700

    feat: add database connection setup

---
+++ cmd/configs/database.go
@@ -0,0 +1,27 @@
+package configs
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	DB = database
+}
--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/ndkode/elabram-backend-recruitment/cmd/configs"
 )
 
 func main() {
 	r := gin.Default()
+
+	// Connect to Database
+	configs.ConnectDB()
 
 	// Run Server
 	r.Run(":8080")
